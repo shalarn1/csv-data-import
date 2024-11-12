fa633ac477d496bd59173417c64ab02f3ed1920a
shalarn1 <sarika.halarnakar@gmail.com>
2024-11-12 01:34:41 -0500

    add inspections

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: inspections
+#
+#  id            :bigint           not null, primary key
+#  score         :integer
+#  occurred_on   :date             not null
+#  category      :integer          not null
+#  restaurant_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Inspection < ApplicationRecord
+	validates :occurred_on, :category, presence: true
+	validates_uniqueness_of :occurred_on, scope: [:restaurant_id, :category]
+	enum category: %i[routine_unscheduled routine_scheduled reinspection_follow_up
+										foodborne_illness_investigation non_inspection_site_visit complaint new_ownership]
+
+	belongs_to :restaurant
 end
--- db/migrate/20241108234829_create_inspections.rb
+++ db/migrate/20241108234829_create_inspections.rb
@@ -1,8 +1,15 @@
 class CreateInspections < ActiveRecord::Migration[7.0]
   def change
     create_table :inspections do |t|
+      t.integer :score
+      t.date :occurred_on, null: false
+      t.integer :category, null: false
 
-      t.timestamps
+      t.references :restaurant, index: true, foreign_key: true, null: false
+
+      t.timestamps null: false
     end
+
+    add_index :inspections, [:restaurant_id, :occurred_on, :category], unique: true, name: :index_inspections_on_search_fields
   end
 end
